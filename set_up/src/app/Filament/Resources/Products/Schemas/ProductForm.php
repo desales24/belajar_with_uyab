7a7e0fb3c457bb61eef56e93cc47b39fb10d6a08
desales24 <fransiskusdes24@gmail.com>
2026-02-19 23:33:47 +0700

    first commit

---
+++ set_up/src/app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Select;
+use App\Models\Category;
+use Filament\Schemas\Schema;
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                Select::make('category_id')
+                    ->options(Category::all()->pluck('name', 'id'))
+                    ->required()
+            ]);
+    }
+}
---
+++ set_up/src/app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'product_category');
+    }
+}
